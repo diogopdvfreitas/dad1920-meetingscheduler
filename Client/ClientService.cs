a0123477abf7862fe0e9c11492fb8fcd1042085f
Rita Prates <ritarodriguesprates@gmail.com>
2019-10-23 18:20:40 +0100

    Client
    ScriptClient serve para ler o script;
    ClientService correpsonde ao objecto remoto do lado do cliente;
    Meeting é uma reunião (não tenho a certeza se precisamos desta classe ou não);

---
+++ Client/ClientService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using RemotingServicesLibrary;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client {
+    public class ClientService : MarshalByRefObject, IClientService {
+    }
+}
---
+++ Client/Meeting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace Client {
+    [Serializable]
+    public class Meeting {
+        String _coord;
+        String _topic;
+        int _nPart;
+        int _nSlots;
+        int _nInvit;
+        ArrayList _slots;
+        ArrayList _invits;
+
+        public Meeting(String topic, int nPart, int nSlots, int nInvit, ArrayList slots, ArrayList invits) {
+            //_coord = coord;
+            _topic = topic;
+            _nPart = nPart;
+            _nSlots = nSlots;
+            _nInvit = nInvit;
+            _slots = slots;
+            _invits = invits;
+        }
+
+    }
+}
