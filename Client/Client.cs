dc2fb6469cdf04c3f27a04967aafd271afbf8fb5
Rita Prates <ritarodriguesprates@gmail.com>
2019-10-23 17:06:55 +0100

    MSDAD project structure created

---
+++ Client/Client.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client {
+    public class Client {
+
+        public void createMeeting() { }
+        static void Main(string[] args) {
+
+        }
+    }
+}
