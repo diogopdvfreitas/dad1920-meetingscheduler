ac969a9c7bd2ab91bdb0c93c1ada6a6c41979dd2
Rita Prates <ritarodriguesprates@gmail.com>
2019-10-26 22:01:11 +0100

    Create Slot.cs

---
+++ ObjectsLibrary/Slot.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsLibrary {
+
+    public class Slot {
+       // private String _place;
+        //private String _date;
+    }
+}
