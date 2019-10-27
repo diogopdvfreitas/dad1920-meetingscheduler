34ea8e62445939375290efb89048112cd14ec74e
Diogo Freitas <diogo.maria.freitas@gmail.com>
2019-10-27 02:16:43 +0000

    Classes Location and Room created
    - Also created script.txt for testing purposes in the future

---
+++ ObjectsLibrary/Location.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsLibrary {
+    public class Location {
+        private String _name;
+        private List<Room> _rooms;
+
+        public Location(String name) {
+            _name = name;
+            _rooms = new List<Room>();
+        }
+
+        public String Name {
+            get { return _name; }
+        }
+
+        public void addRoom(Room room) {
+            _rooms.Add(room);
+        }
+    }
+}
---
+++ ObjectsLibrary/Room.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsLibrary {
+    public class Room {
+        private String _name;
+        private int _capacity;
+        private IDictionary<String, String> _meetingSchedule; // Key - Meeting Topic; Value - Meeting Date
+
+        public Room(String name, int capacity) {
+            _name = name;
+            _capacity = capacity;
+            _meetingSchedule = new Dictionary<String, String>();
+        }
+
+        public String Name {
+            get { return _name; }
+        }
+
+        public int Capacity {
+            get { return _capacity; }
+        }
+
+        public void bookMeeting(Meeting meeting, String date) {
+            _meetingSchedule.Add(meeting.Topic, date);
+        }
+    }
+}
